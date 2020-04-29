6d93d7c690957338d88613229c0f433c0ac8e4f1
Bradley Kingsley <bradleykigsley2@gmail.com>
2020-04-30 00:26:34 +0300

    add images

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Page3 = props => {
+    return (
+        <div>
+            <h2>I bring the pasta.</h2>
+            <p>Page 3</p>
+        </div>
+    );
+};
+
+export default Page3;
